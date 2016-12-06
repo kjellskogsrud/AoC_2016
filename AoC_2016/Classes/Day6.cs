f869f78d980c210942be86df987f6ced1f60e202
Kjell Skogsrud <kjell@skogsrud.net>
2016-12-06 07:36:23 +0100

    begin day 6

---
+++ AoC_2016/Classes/Day6.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_2016.Classes
+{
+    class Day6
+    {
+        // Somewhere to store our bacis input
+        public string[] input;
+
+        public Day6()
+        {
+            // Read input from file into the array
+            input = File.ReadAllLines(@"Inputs\Day6.txt");
+
+        }
+        public void Solve()
+        {
+
+        }
+
+    }
+}
--- AoC_2016/Program.cs
+++ AoC_2016/Program.cs
@@ -11,9 +11,13 @@ namespace AoC_2016
     {
         static void Main(string[] args)
         {
+            // Day 6 http://adventofcode.com/2016/day/6
+            Day6 day6 = new Day6();
+            day6.Solve();
+
             // Day 5 http://adventofcode.com/2016/day/5
-            Day5 day5 = new Day5();
-            day5.Solve();
+            //Day5 day5 = new Day5();
+            //day5.Solve();
 
             // Day 4 http://adventofcode.com/2016/day/4
             //Day4 day4 = new Day4();
