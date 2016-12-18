582521d7deaf9f4cdfb98af4bc195cd33c9aa222
unknown <NOKJSKO1@NO-L-7000867.nmea.abb.com>
2016-12-18 09:00:15 +0100

    Finally solved day 9 part 2.

--- AoC_2016/Classes/Day9.cs
+++ AoC_2016/Classes/Day9.cs
@@ -21,7 +21,78 @@ namespace AoC_2016.Classes
 
         public void Solve()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Counted: {0}",Decompress(input[0]));
+        }
+
+        private double Decompress(string inputString)
+        {
+            double charCount = 0;
+            bool isMarker = false;
+            int subsequentChars = 1;
+            int repeat = 1;
+            string currentMarker = "";
+
+            // Check each char
+            for (int c = 0; c < inputString.Length; c++)
+            {
+                // Detect start and end of markers
+                if (inputString[c] == '(' && currentMarker == "")
+                {
+                    isMarker = true;
+                    continue;
+                }
+                else if (inputString[c] == ')')
+                {
+                    isMarker = false;
+                    continue;
+                }
+
+                // What to do with this char?
+                // If we are not in a marker
+                if (!isMarker)
+                {
+                    // So we are currently not in a marker
+                    // Have we an unparsed marker?
+                    if(currentMarker != "")
+                    {
+                        subsequentChars = int.Parse(currentMarker.Split('x')[0]);
+                        repeat = int.Parse(currentMarker.Split('x')[1]);
+                        currentMarker = "";
+                    }
+
+                    // Then if subsequent chars is greater than 5
+                    if(subsequentChars > 5)
+                    {
+                        // then we have a substring, that could possible contain another marker
+                        // If so we should decompress that
+                        string substring = inputString.Substring(c, subsequentChars);
+                        charCount = charCount + (Decompress(substring) * repeat);
+                        
+                        // And then we skip the string we just handed to Decompress.
+                        c = c + (subsequentChars-1);
+
+                        // Then we reset the subsequent chars to 1.
+                        subsequentChars = 1;
+                        repeat = 1;
+
+                        // And move on to the next char
+                        continue;
+                    }
+                    // But if we dont have more than 5 chars, then it can not hold another marker,
+                    else
+                    {
+                        charCount = charCount + (subsequentChars * repeat);
+                        c = c + (subsequentChars - 1);
+                        subsequentChars = 1;
+                        repeat = 1;
+                    }
+                }
+                else
+                {
+                    currentMarker = currentMarker + inputString[c].ToString();
+                }
+            }
+            return charCount;
         }
     }
 }
